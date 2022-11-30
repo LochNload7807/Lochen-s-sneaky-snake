681d719e4896b5309b6388011924f22d24d2859e
Lochen Foltz <foltzlw@s.Dcsdk12.org>
2022-11-30 16:29:03 -0700

    11/30

--- scripts/app.js
+++ scripts/app.js
@@ -15,13 +15,20 @@ class Player {
 	/**
 	 * @param {number} x
 	 * @param {number} y
+	 * @param {CanvasRenderingContext2D} ctx
 	 */
-	constructor(x, y) {
+	constructor(x, y, ctx) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
-		this.segments = [];
+		this.ctx = ctx;
+		this.segments = [new Segment(this.x, this.y, "yellow", ctx)];
+	}
+
+	update() {}
+	draw() {
+		this.segments.forEach((s) => {});
 	}
 }
 
@@ -30,16 +37,36 @@ class Segment {
 	 * @param {number} x
 	 * @param {number} y
 	 * @param {string} color
+	 * @param {CanvasRenderingContext2D} ctx
 	 */
-	constructor(x, y, color) {
+	constructor(x, y, color, ctx) {
 		this.x = x;
 		this.y = y;
 		this.w = game.gridSize;
 		this.h = this.w;
 		this.color = color;
+		this.ctx = ctx;
 	}
 
 	update() {}
 
-	draw() {}
+	draw() {
+		this.ctx.fillStyle = this.color;
+		this.ctx.fillRect(this.x, this.y, this.w, this.h);
+	}
 }
+
+let p1 = new Player(5 * game.gridSize, 5 * game.gridSize, ctx);
+
+let currentTime = 0;
+
+function gameLoop(timestamp) {
+	let elaspedTime = timestamp - currentTime;
+	currentTime = timestamp;
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
